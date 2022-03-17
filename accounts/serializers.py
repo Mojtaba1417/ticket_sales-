4aa707659e1d8317dad003d628b7aeb61a7abb95
Mojtaba <mojtaba.1417@yahoo.com>
2022-03-17 15:47:57 +0330

    test

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=ProfileModel
-        fields=["username","password","email","credit","Gender"]
+        fields=["username","password","email","credit","Gender","ProfileImage"]
 
  
